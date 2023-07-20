5050c5db7f145ee1a3a61c6d13713b3319f0e9a6
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-07-20 19:29:31 +0500

    Добавьте файлы проекта.

---
+++ WpfAppClientToApi/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppClientToApi
+{
+    class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ WpfAppClientToApi/Presenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppClientToApi
+{
+    class Presenter
+    {
+        View View;
+        ModelLogic Logic;
+
+        public Presenter(View view)
+        {
+            Logic = new ModelLogic();
+            View = view;
+        }
+
+    }
+}
